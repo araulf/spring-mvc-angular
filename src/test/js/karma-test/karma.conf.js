7b7b843b7960f80434890267318ee69b711c8819
glawler <lawlerg@LAPTOP1035.V1.com>
2015-08-05 16:56:52 +0100

    remove unused dependency

--- src/test/js/karma-test/karma.conf.js
+++ src/test/js/karma-test/karma.conf.js
@@ -2,10 +2,9 @@ module.exports = function(config) {
   config.set({
     basePath: '.',
     browsers: ['PhantomJS'],
-    frameworks: ['jasmine', 'browserify'],
+    frameworks: ['jasmine'],
     files: ['*.spec.js'],
-    plugins: ['karma-browserify',
-              'karma-jasmine',
+    plugins: ['karma-jasmine',
               'karma-phantomjs-launcher'],
     singleRun: true
   });
