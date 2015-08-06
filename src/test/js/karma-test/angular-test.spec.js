9fc3daeb8416e735d27fd01ebdaedc3b2a4f8dde
glawler <lawlerg@LAPTOP1035.V1.com>
2015-08-06 10:56:13 +0100

    add working jasmine test

--- src/test/js/karma-test/angular-test.spec.js
+++ src/test/js/karma-test/angular-test.spec.js
@@ -2,13 +2,21 @@ describe("Angular tests", function() {
 
     beforeEach(module('angularTestApp'));
 
-    var $controller;
+    var controller;
+    var scope;
+    var httpBackend;
 
-    beforeEach(inject(function(_$controller_){
-        $controller = _$controller_;
+    beforeEach(inject(function($rootScope, $controller, $httpBackend){
+        scope = $rootScope.$new();
+        httpBackend = $httpBackend;
+        httpBackend.when("GET", "api/hello").respond("Hello");
+        controller = $controller('TestController', {$scope: scope});
     }));
 
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+    it("calls the hello api", function() {
+        httpBackend.expectGET('api/hello').respond(200, 'Hello');
+        scope.getHello();
+        httpBackend.flush();
+        expect(scope.result).toBe("Hello");
     });
 });
